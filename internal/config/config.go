17a358ad655111293d2f211305fcd3ca19c6057a
Cristian Radulescu <cristian67@gmail.com>
2025-10-21 22:34:28 +0300

    v0.2.0

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	Name           string = "php-diagls"
-	Version        string = "0.1.0"
+	Version        string = "0.2.0"
 	ConfigFileName string = ".php-diagls.json"
 
 	ConfigItemDiagnosticsProviders string = "diagnosticsProviders"
